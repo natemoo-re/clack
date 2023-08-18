ef9f301de0a93586aafdf88660a294af1463c10e
Bruno Rocha <brunorocha2674@gmail.com>
2023-08-18 14:21:32 -0300

    feat: format with minWith

--- packages/core/src/prompts/prompt.ts
+++ packages/core/src/prompts/prompt.ts
@@ -130,6 +130,7 @@ export interface FormatOptions extends Record<LineOption, Partial<FormatLineOpti
 	 * //=> 'foo bar\nbaz'
 	 */
 	maxWidth: number;
+	minWidth: number;
 }
 
 export default class Prompt {
@@ -324,6 +325,7 @@ export default class Prompt {
 			) + 2;
 		const terminalWidth = process.stdout.columns || 80;
 		const maxWidth = options?.maxWidth ?? terminalWidth;
+		const minWidth = options?.minWidth ?? 1;
 
 		const formattedLines: string[] = [];
 		const paragraphs = text.split(/\n/g);
@@ -380,7 +382,9 @@ export default class Prompt {
 				const styledLine = leadingSpaceRegex.test(line)
 					? ' ' + styleLine(line.slice(1))
 					: styleLine(line);
-				return [startLine, styledLine, endLine].join(' ');
+				const fullLine =
+					styledLine + ' '.repeat(Math.max(minWidth - strLength(styledLine) - emptySlots, 0));
+				return [startLine, fullLine, endLine].join(' ');
 			})
 			.join('\n');
 	}
